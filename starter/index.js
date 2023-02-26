77b20940fc6959bda8fcd758cef7815ad548bfde
lissasimp <116848303+lissasimp@users.noreply.github.com>
2023-02-26 11:41:35 +0000

    validation added to user questions

--- starter/index.js
+++ starter/index.js
@@ -14,38 +14,69 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 //manager questions - pseudocode from Office hours tutor
-
+const promptForManager = () => {
 inquirer
 .prompt([
     {
         type: "input",
         message: "Manager's Name?",
         name: "managerName",
+        //https://pakstech.com/blog/inquirer-js/?utm_content=cmp-true
+        validate(answer) {
+            if(!answer) {
+                return "Please fill in this information"
+            }
+            return true
+        }
     },
     {
         type: "input",
         message: "Manager's Id?",
         name: "managerID",
+        validate(answer) {
+            if(!answer) {
+                return "Please fill in this information"
+            }
+            return true
+        }
     },
     {
         type: "input",
         message: "Manager's Email?",
         name: "managerEmail",
-    },
+       // https://stackoverflow.com/questions/65189877/how-can-i-validate-that-a-user-input-their-email-when-using-inquirer-npm
+        validate: function(email)
+        {
+            // Regex mail check (return true if valid mail)
+            return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email);
+        }
+    }, 
     {
         type: "input",
         message: "Manager's Office Number?",
         name: "managerOfficeNumber",
+        validate(answer) {
+            if(!answer) {
+                return "Please fill in this information"
+            }
+            return true
+        }
     },
+
 
     ]).then(response => {
     // populate manager info  - pseudocode from Office hours tutor
     // promptForNexEmployee ()  - pseudocode from Office hours tutor
-    promptForNextEmployee();
-})
+
+    promptForNextEmployee()
+    }
+)};
 
 const promptForNextEmployee = () => {
     inquirer.prompt([
+        //do you want to add another employee?
+        //if yes
+        //if no build page?
         // choice of 3  - pseudocode from Office hours tutor
         {
             type: "list",
@@ -53,14 +84,22 @@ const promptForNextEmployee = () => {
             name: "employeeRole",
         },
 ]).then(response => {
-        // if engineer  - pseudocode from Office hours tutor
+        if ("Engineer") {
+            promptForEngineer()
+          //pseudocode from Office hours tutor
         //    promptForEngineer
         // else if intern
-        //    promptForIntern
-        // else
+        } else if ("Intern") {
+        promptForIntern ()
+        } else if ("Manager") {
+            promptForManager()
+        } else {
+            
+        }
         //    use the functionality from page-template to generate the team  - pseudocode from Office hours tutor
     })
 }
+
 
 const promptForEngineer = () => {
     inquirer.prompt([
@@ -69,21 +108,44 @@ const promptForEngineer = () => {
             type: "input",
             message: "Engineer's Name?",
             name: "engineerName",
+            validate(answer) {
+                if(!answer) {
+                    return "Please fill in this information"
+                }
+                return true
+            }
         },
         {
             type: "input",
             message: "Engineer's Id?",
             name: "engineerID",
+            validate(answer) {
+                if(!answer) {
+                    return "Please fill in this information"
+                }
+                return true
+            }
         },
         {
             type: "input",
             message: "Engineer's Email?",
             name: "engineerEmail",
+            validate: function(email)
+            {
+                // Regex mail check (return true if valid mail)
+                return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email);
+            }
         },
         {
             type: "input",
             message: "Engineer's Github?",
             name: "engineerGithub",
+            validate(answer) {
+                if(!answer) {
+                    return "Please fill in this information"
+                }
+                return true
+            }
         },
     
     ]).then(response => {
@@ -99,21 +161,44 @@ const promptForIntern = () => {
             type: "input",
             message: "Intern's Name?",
             name: "internName",
+            validate(answer) {
+                if(!answer) {
+                    return "Please fill in this information"
+                }
+                return true
+            }
         },
         {
             type: "input",
             message: "Intern's Id?",
             name: "internID",
+            validate(answer) {
+                if(!answer) {
+                    return "Please fill in this information"
+                }
+                return true
+            }
         },
         {
             type: "input",
             message: "Intern's Email?",
             name: "internEmail",
+            validate: function(email)
+        {
+            // Regex mail check (return true if valid mail)
+            return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email);
+        }
         },
         {
             type: "input",
             message: "Intern's School?",
             name: "internSchool",
+            validate(answer) {
+                if(!answer) {
+                    return "Please fill in this information"
+                }
+                return true
+            }
         },
     ]).then(response => {
         // add new intern to employees array  - pseudocode from Office hours tutor
@@ -124,3 +209,12 @@ const promptForIntern = () => {
 const buildPage = () => {
 // render(myArrayOfTeamMembers)  - pseudocode from Office hours tutor
 }
+
+// const songSnippet = `
+//   <div class="song">
+// <h2>Title: ${music.title}</h2>
+// <p>Artist: ${music.artist} </p>
+// <p>Album:  ${music.album} </p>
+
+// <h2>${music.title}<span> by ${music.artist}</span></p>
+//   </div>
